aaa2d9c5860f5dfb08400edc5306c6359c8fdd66
Curtis Chen <curtischenjr@gmail.com>
2026-06-23 10:59:54 -0700

    unet

--- models/unet.py
+++ models/unet.py
@@ -6,7 +6,7 @@ class TimeEmbedding(nn.Module):
     def __init__(self, d_in, d_out):
         super().__init__()
         self.proj1 = nn.Linear(d_in, d_out)
-        self.relu = nn.ReLU()
+        self.silu = nn.SiLU()
         self.proj2 = nn.Linear(d_out, d_out)
         self.d_in = d_in
 
@@ -17,21 +17,30 @@ class TimeEmbedding(nn.Module):
         for i in range(1, self.d_in, 2):
             time_embedding[:, i] = math.cos(t / 10000 ** (i / self.d_in))
         
-        return self.proj2(self.relu(self.proj1(time_embedding)))
+        return self.proj2(self.silu(self.proj1(time_embedding)))
 
 class ConvBlock(nn.Module):
     def __init__(self, d_in):
         super().__init__()
         self.d_in = d_in
-        self.conv1 = nn.Conv2d(d_in, d_in // 2, 3, stride=2)
-        self.norm1 = nn.BatchNorm2d(d_in // 2)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(d_in // 2, d_in // 4, 3, stride=2)
-        self.norm2 = nn.BatchNorm2d(d_in // 4)
-        self.relu2 = nn.ReLU()
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=1)
+        self.norm1 = nn.GroupNorm(8, 32)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1)
+        self.norm2 = nn.GroupNorm(8, 64)
+        self.x = nn.Linear(28, 7)
+        self.silu1 = nn.SiLU()
     
-    def forward(self, image):
-        
+    def forward(self, image, time):
+        image = self.norm1(self.conv1(image))
+        image = self.norm2(self.conv2(image))
+        image = self.silu1(self.x(image))
+
+        time_embedding = TimeEmbedding(28, 7).forward(time)
+
+        image = image + time_embedding
+        return image
+
+
         
 
 
