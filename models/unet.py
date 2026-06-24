544a12762ecf2a044f6852e8a8d2508bbb325b25
Curtis Chen <curtischenjr@gmail.com>
2026-06-24 16:46:53 -0700

    unet

--- models/unet.py
+++ models/unet.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import torch.nn as nn
 import torch
 import math
@@ -20,25 +22,34 @@ class TimeEmbedding(nn.Module):
         return self.proj2(self.silu(self.proj1(time_embedding)))
 
 class ConvBlock(nn.Module):
-    def __init__(self, d_in):
+    def __init__(self, in_channels, out_channels, time_emb_dim, downsample=False):
         super().__init__()
-        self.d_in = d_in
-        self.conv1 = nn.Conv2d(1, 32, 3, stride=1)
-        self.norm1 = nn.GroupNorm(8, 32)
-        self.conv2 = nn.Conv2d(32, 64, 3, stride=1)
-        self.norm2 = nn.GroupNorm(8, 64)
-        self.x = nn.Linear(28, 7)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.time_emb_dim = time_emb_dim
+        self.downsample = downsample
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1, stride=2 if downsample else 1)
+        self.norm1 = nn.GroupNorm(8, out_channels)
         self.silu1 = nn.SiLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1, stride=1)
+        self.norm2 = nn.GroupNorm(8, out_channels)
+        self.silu2 = nn.SiLU()
+        self.time_proj = nn.Linear(time_emb_dim, out_channels)
     
     def forward(self, image, time):
-        image = self.norm1(self.conv1(image))
-        image = self.norm2(self.conv2(image))
-        image = self.silu1(self.x(image))
-
-        time_embedding = TimeEmbedding(28, 7).forward(time)
-
-        image = image + time_embedding
+        image = self.silu1(self.norm1(self.conv1(image)))
+        t = self.time_proj(time)[:, :, None, None]
+        image = image + t
+        image = self.silu2(self.norm2(self.conv2(image)))
         return image
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, time_emb_dim):
+        super().__init__()
+        self.convblock = ConvBlock(in_channels=in_channels, out_channels=out_channels, time_emb_dim=time_emb_dim)
+        self.upsample = nn.Upsample()
+
+    def forward(self, image, skip, time):
 
 
         
