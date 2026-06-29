7b1acf3079dc859be9ff4b7f72f00a56bfb41966
curtisjchen <curtischenjr@gmail.com>
2026-06-28 19:25:12 -0700

    flow matching loss

---
+++ flow.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn.functional as F
+
+def flow_matching_loss(model, x_1):
+    x_0 = torch.randn_like(x_1)
+    t = torch.rand(x_1.shape[0], device=x_1.device)
+    t_reshaped = t.reshape(-1, 1, 1, 1)
+    x_t =  (1-t_reshaped) * x_0 + t_reshaped * x_1
+    v_star = x_1 - x_0 # this is the target
+    v_theta = model(x_t, t)# this is our model 
+    return F.mse_loss(v_theta, v_star)
